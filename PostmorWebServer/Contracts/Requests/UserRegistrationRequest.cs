7bbb3dbebdf4b35ef01ebd1c3fc6d5dd8d7d975b
Alexander <mystige@hotmail.com>
2019-12-27 13:15:19 +0100

    Added request models
    Added request models for login, register and token refresh requests

---
+++ PostmorWebServer/Contracts/Requests/UserRegistrationRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PostmorWebServer.Contracts.Requests
+{
+    public class UserRegistrationRequest
+    {
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public string Picture { get; set; }
+
+    }
+}
