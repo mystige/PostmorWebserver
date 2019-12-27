55243d2c87052279ee232cf2396422b31486dea4
Alexander <mystige@hotmail.com>
2019-12-27 13:25:13 +0100

    Restructured file hierarchy + added RefreshToken entity

---
+++ PostmorWebServer/Data/Entities/RefreshToken.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PostmorWebServer.Data.Entities
+{
+    public class RefreshToken
+    {
+        [Key]
+        public string Token { get; set; }
+        public string JwtID { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime Expirydate { get; set; }
+        public bool Used { get; set; }
+        public bool Invaildated { get; set; }
+        public string UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; }
+    }
+}
