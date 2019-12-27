d499e62fa4ef4c17f41ba740774b6eb3f0353efd
Alexander <mystige@hotmail.com>
2019-12-27 13:21:22 +0100

    Added class containing all Routes
    Added a Static file containing all the Routes for the API

---
+++ PostmorWebServer/Contracts/ApiRoutes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PostmorWebServer.Contracts
+{
+    public class ApiRoutes
+    {
+        public static class Identity
+        {
+            public const string Login = "/identity/login";
+            public const string Register = "/identity/register";
+            public const string Refresh = "/identity/refresh";
+        }
+    }
+}
