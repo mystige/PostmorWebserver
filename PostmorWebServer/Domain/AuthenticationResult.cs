0997ab27fc994fceed08e02d0636ab837a3b768b
Alexander <mystige@hotmail.com>
2019-12-27 13:33:55 +0100

    Added a model for AuthResults

---
+++ PostmorWebServer/Domain/AuthenticationResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PostmorWebServer.Domain
+{
+    public class AuthenticationResult
+    {
+        public string Token { get; set; }
+        public string RefreshToken { get; set; }
+        public bool Succes { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
