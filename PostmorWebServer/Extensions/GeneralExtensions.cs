ae201f318a706f76df0492b83fe7056fc06bb015
Alexander <mystige@hotmail.com>
2019-12-27 12:51:18 +0100

    Added GeneralExtensions class
    Extensions for extraction the Id of a user from a claim

---
+++ PostmorWebServer/Extensions/GeneralExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Linq;
+
+namespace PostmorWebServer.Extensions
+{
+    //Extensions for extraction the Id of a user from a claim
+    public static class GeneralExtensions
+    {
+        public static string GetUserId(this HttpContext httpContext)
+        {
+            if (httpContext.User == null)
+            {
+                return string.Empty;
+            }
+            return httpContext.User.Claims.Single(x => x.Type == "id").Value;
+        }
+    }
+}
