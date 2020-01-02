9e896bda2f6961c356c4a848e2e86c805fd4bea1
Alexander <mystige@hotmail.com>
2020-01-02 10:21:43 +0100

    Changed "Success to Exists"

---
+++ PostmorWebServer/Domain/UserCard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PostmorWebServer.Domain
+{
+    public class UserCard
+    {
+        public bool Exists { get; set; }
+        public string Error{ get; set; }
+        public int ContactId { get; set; }
+        public string ContactName { get; set; }
+        public string Picture { get; set; }
+        public bool IsFriend { get; set; }
+        public string PublicKey { get; set; }
+    }
+
+}
