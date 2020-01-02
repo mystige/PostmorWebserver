cb9f99d7ff824f6459554e5d82b45392fba3d2db
Alexander <mystige@hotmail.com>
2020-01-02 08:39:59 +0100

    Added a Class containing Contact logic

---
+++ PostmorWebServer/Services/ContactService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PostmorWebServer.Data;
+using PostmorWebServer.Domain;
+
+namespace PostmorWebServer.Services
+{
+    public class ContactService : IContactService
+    {
+        private readonly DataContext _dbContext;
+
+        public ContactService(DataContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> AddAsync(int RequesterId, int ContactId)
+        {
+            var user = await _dbContext.Users.SingleOrDefaultAsync(x => x.Id == RequesterId);
+            var contact = await _dbContext.Users.SingleOrDefaultAsync(x => x.Id == ContactId);
+            if (user == null || contact == null)
+            {
+                return false;
+            }
+            user.Contacts.Add(contact);
+            var updated = await _dbContext.SaveChangesAsync();
+            return updated > 0 ? true : false;
+        }
+
+        public async Task<UserCard> GetUserAsync(int RequesterId, int ContactID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<UserCard> GetUserAsync(int RequesterId, string ContactAddress)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> RemoveAsync(int RequesterId, int ContactId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PostmorWebServer/Services/IContactService.cs
@@ -0,0 +1,17 @@
+﻿using PostmorWebServer.Data.Entities;
+using PostmorWebServer.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PostmorWebServer.Services
+{
+    public interface IContactService
+    {
+        Task<bool> AddAsync(int RequesterId, int ContactId);
+        Task<bool> RemoveAsync(int RequesterId, int ContactId);
+        Task<User> FindUserByIdAsync(int Id);
+        Task<User> FindUserByAddressAsync(string Address);
+    }
+}
